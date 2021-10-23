f9bf2fc9371a3bb868d2c132bb2af75940dbc73c
Rina Volovich <rinav@Rinas-MacBook-Air.local>
2021-10-23 20:43:08 +0300

    access control origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true })); //parse url data of type encode
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/contact');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	next();
